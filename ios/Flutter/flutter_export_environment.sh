805a0baa5c1ad25a1e01ba62ec2a30a967c2012b
md sadab wasim <mdsadabwasim007@gmail.com>
2019-12-30 11:45:32 +0530

    new target var for testing

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,7 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/inndata_flutter/Documents/devlopment/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/inndata_flutter/Documents/GitHub/Toughest"
-export "FLUTTER_TARGET=/Users/inndata_flutter/Documents/GitHub/Toughest/lib/main.dart"
+export "FLUTTER_TARGET=/Users/inndata_flutter/Documents/GitHub/Toughest/test_driver/app.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/inndata_flutter/Documents/devlopment/flutter/bin/cache/artifacts/engine/ios"
