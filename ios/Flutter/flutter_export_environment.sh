45d9b448515b8712d4d2ef3066536fe69787b5af
mdsadabwasim <mdsadabwasim007@gmail.com>
2022-09-04 10:39:52 +0530

    Migrated to android embedding 2.0 & updated plugins

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/creator/Documents/Soft/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/creator/AndroidStudioProjects/Toughest"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\sadab\Documents\projects\Toughest"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/creator/Documents/Soft/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=2.0.2"
 export "FLUTTER_BUILD_NUMBER=4"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
